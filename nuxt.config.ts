bae43343982b6fdb390a48be601ddcdff496c2e4
Nobuhito ARAI <nobht_arai@icloud.com>
2022-05-03 21:17:04 +0900

    :wrench: add type directory and alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
+import path from 'path'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules:[
-        '@pinia/nuxt',
-    ],
-  ssr: true
+  buildModules: ['@pinia/nuxt'],
+  ssr: true,
+  alias: {
+    '@type': path.resolve(__dirname) + 'type.d',
+  },
 })
