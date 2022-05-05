62fbe21fdfc99c42b5b9e18f0cfd65b1685f35b8
Nobuhito ARAI <nobht_arai@icloud.com>
2022-05-05 10:26:18 +0900

    :fire: remove vant.

--- plugins/vant.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin } from 'nuxt/app'
-import { Row, Col, Icon, Image, Cell, CellGroup } from 'vant'
-
-import 'vant/lib/index.css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Row).use(Col).use(Image).use(Icon).use(Cell).use(CellGroup)
-})
