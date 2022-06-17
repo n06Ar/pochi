2e0f6c1ef4a32fc5de1f9503e29a6ae34cbb5bfa
Nobuhito ARAI <nobht_arai@icloud.com>
2022-06-17 15:56:46 +0900

    :sparkles: add default template.

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <Toolbar><h1>Pochi</h1></Toolbar>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer></footer>
+</template>
+
+<script setup lang="ts">
+import { useCommonStore } from '@store'
+import Toolbar from 'primevue/toolbar'
+
+const { title } = useCommonStore()
+</script>
+
+<style>
+.header {
+}
+</style>
